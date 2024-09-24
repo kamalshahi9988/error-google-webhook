f5e673b8442ffea06e15deb22600c37d11b6376f
kamal-shahi <kamal.shahi9988@gmail.com>
2024-09-25 01:50:58 +0545

    Fix: add env and auth

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const axios = require(`axios`);
+require("dotenv").config();
 
 async function reporterMiddleware(req, res, next) {
   try {
-    console.log("Test");
-
-    const WEBHOOK_URL = "process.env.WEBHOOK_URL";
-    // const WEBHOOK_URL = process.env.WEBHOOK_URL;
-
-    const text = `Hello test message here`;
-    const response = await axios.post(WEBHOOK_URL, {
-      text: text,
-    });
-    console.log(response);
-    next();
+    const WEBHOOK_URL = process.env.WEBHOOK_URL;
+    if (WEBHOOK_URL) {
+      const text = `Hello test message here`;
+      const response = await axios.post(WEBHOOK_URL, {
+        text: text,
+      });
+      console.log(response);
+      return next();
+    }
+    throw new Error("Add WEBHOOK_URL as env variable");
   } catch (err) {
-    console.log(err);
-    throw new Error(err);
+    return next(err);
   }
 }
 
